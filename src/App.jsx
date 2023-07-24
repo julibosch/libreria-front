d00d9d1a737415955b2c31f261f005f0c7cb9b98
JuliBosch <julibosch99@gmail.com>
2023-07-24 16:43:01 -0300

    "comienzo de 0"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import Header from "./components/Header"
-import Sidebar from "./components/Sidebar"
-
 function App() {
   return (
     <div className="h-screen flex">
-      <Sidebar />
-      <div className="w-full">
-        <Header
-          title={"Inicio"}
-        />
-      </div>
+      
     </div>
   )
 }
